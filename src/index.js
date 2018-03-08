b1df0dc2f9d2f66fea2bff93df1a3890ba2de14d
kamal <kamalcdsk@gmail.com>
2018-03-08 11:20:09 +0530

    translation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ import App from './App';
 import en from 'react-intl/locale-data/en';
 import fr from 'react-intl/locale-data/fr';
 import de from 'react-intl/locale-data/de';
+import {initializePhraseAppEditor} from 'react-intl-phraseapp'
+
+var config = {
+  projectId: '1f0441795ce0e2b244d6e53a9e3f594e',
+  phraseEnabled: true,
+  prefix: '[[__',
+  suffix: '__]]',
+  fullReparse: true,
+  forceLocale: "pt-BR"
+}
+
+initializePhraseAppEditor(config);
 
 addLocaleData([...en, ...fr, ...de]);
 
